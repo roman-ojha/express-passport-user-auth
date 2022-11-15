89e7e71366bb8ac81fe37877b3e94aecd5003b62
Roman-Ojha <razzroman98@gmail.com>
2022-11-15 22:13:57 +0545

    build: Init Project for Passport Local Strategy

---
+++ 05_Passport_Local_Strategy/config/database.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+require("dotenv").config();
+
+/**
+ * -------------- DATABASE ----------------
+ */
+
+/**
+ * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place the following
+ * string into the `.env` file
+ *
+ * DB_STRING=mongodb://<user>:<password>@localhost:27017/database_name
+ */
+
+const db = "mongodb://localhost:27017/passport-user-auth-tut";
+
+const connection: void & mongoose.Connection = (
+  mongoose as any
+).createConnection(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
+const UserSchema = new mongoose.Schema({
+  username: String,
+  hash: String,
+  salt: String,
+});
+
+const User = connection.model("User", UserSchema);
+
+// Expose the connection
+module.exports = connection;
